76f3b4baac42db080db07c9942b375a3b988ca01
sasiri-jambugaswatta <jambugaswattasasiri@gmail.com>
2025-02-07 19:44:04 +0530

    create factory contract and test with fee and owner

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,4 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.27;
 
-contract Factory {}
+import {Token} from "./Token.sol";
+
+contract Factory {
+	uint256 public immutable fee;
+	address public owner;
+
+	constructor(uint256 _fee) {
+		fee = _fee;
+		owner = msg.sender;
+	}
+
+	
+}
--- test/Factory.js
+++ test/Factory.js
@@ -4,4 +4,27 @@ const { ethers } = require("hardhat")
 
 describe("Factory", function () {
 
+      const FEE = ethers.parseUnits("0.01", "ether");
+
+      async function deployFactoryFixture() {
+            const [deployer] = await ethers.getSigners();
+
+            const Factory = await ethers.getContractFactory("Factory")
+            const factory = await Factory.deploy(FEE);
+            return { factory, deployer }
+      }
+
+
+      describe("Deployment", function () {
+            it("Should set the fee", async function () {
+              const { factory } = await loadFixture(deployFactoryFixture)
+              expect(await factory.fee()).to.equal(FEE)
+            })
+        
+            it("Should set the owner", async function () {
+              const { factory, deployer } = await loadFixture(deployFactoryFixture)
+              expect(await factory.owner()).to.equal(deployer.address)
+            })
+          })
+     
 })
